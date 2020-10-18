a224d26feb69932629d1ea0e3c0f79300332a657
kagol <kagol@sina.com>
2020-10-18 13:33:11 +0800

    fix: 打包增加gzip

--- scripts/tar.js
+++ scripts/tar.js
@@ -6,6 +6,6 @@ fs.stat('./result', (error, stats) => {
     fs.mkdirSync('./result');
   }
 
-  tar.c(null, ['dist', './scripts/deploy-devui.js'])
+  tar.c({ gzip: true }, ['dist', './scripts/deploy-devui.js'])
   .pipe(fs.createWriteStream('./result/devui.tar.gz'));
 });
